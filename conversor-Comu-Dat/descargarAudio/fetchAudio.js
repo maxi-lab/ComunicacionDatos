f912a9a4faf8135a9c9e0d1df4ef8803a118b691
Valentino Chiappini <valentino.chiappini10@gmail.com>
2025-06-20 18:12:08 -0300

    Solucion de errores (sigue habiendo un error con el ffmpeg)

--- conversor-Comu-Dat/descargarAudio/fetchAudio.js
+++ conversor-Comu-Dat/descargarAudio/fetchAudio.js
@@ -5,20 +5,26 @@ response=fetch("http://127.0.0.1:8000/audio/").then(response => {
     }}).then(response => response.blob())
 */
 
+import * as FFmpeg from '@ffmpeg/ffmpeg';
+const { createFFmpeg, fetchFile } = FFmpeg;
+
+//const ffmpeg = new FFmpeg({ log: true });
+//const ffmpeg = createFFmpeg({ log: true });
 
 async function fetchUltimoAudio() {
-    const response = await fetch('http://127.0.0.1:8000/audio/');
+    const response = await fetch('http://127.0.0.1:8000/ultimo-audio/');
     const data = await response.json();
     const audioBlob = await fetch(data.audio_url).then(r => r.blob());
     return audioBlob;
 }
 
 
-const { createFFmpeg, fetchFile } = FFmpeg;
-const ffmpeg = createFFmpeg({ log: true });
+//const { createFFmpeg, fetchFile } = FFmpeg;
+
+
 
 async function convertirYDescargar(formato) {
-    if (!ffmpeg.isLoaded()) await ffmpeg.load();
+    if (!ffmpeg.isLoaded()) { await ffmpeg.load();}
 
     const webmBlob = await fetchUltimoAudio();
     const webmFile = new File([webmBlob], 'audio.webm');
@@ -39,6 +45,9 @@ async function convertirYDescargar(formato) {
     ffmpeg.FS('unlink', 'audio.webm');
     ffmpeg.FS('unlink', outputFile);
 }
+
+
+//await ffmpeg.load(); // Carga FFmpeg antes de usarlo
 
 //Botones para descargar
 document.getElementById("wav").addEventListener("click", () => {
