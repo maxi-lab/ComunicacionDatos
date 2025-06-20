5862ede0eaefb29204a694d11a1c9abcddedb961
Maximo Preneste <maxipreneste@gmail.com>
2025-06-20 16:13:28 -0300

    llamar a api

--- conversor-Comu-Dat/descargarAudio/fetchAudio.js
+++ conversor-Comu-Dat/descargarAudio/fetchAudio.js
@@ -1,4 +1,9 @@
-response=fetch("http://127.0.0.1:8000/audio/").then(response => {
-    for (const header of response.headers) {
-        console.log(header);
-    }}).then(response => response.blob())
+const btn=document.getElementById("convertir");
+btn.addEventListener("click", async () => {
+fetch("http://127.0.0.1:8000/audio/").then(response => {
+    console.log("Response received:", response);
+
+    }).catch((error) => {
+    console.error("Error fetching audio:", error);
+    });
+})
